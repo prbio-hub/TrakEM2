e3a43650619a7f654451b60d720a423bf96e1f36
Stefan Posch <stefan.posch@informatik.uni-halle.de>
2019-05-12 22:04:22 +0200

    added writing all layers ; minimum radius of 1 for all nodes

--- src/main/java/de/unihalle/informatik/rhizoTrak/addon/RhizoWriteBinary.java
+++ src/main/java/de/unihalle/informatik/rhizoTrak/addon/RhizoWriteBinary.java
@@ -76,26 +76,24 @@ import de.unihalle.informatik.MiToBo.apps.minirhizotron.utils.Drawing;
 import de.unihalle.informatik.MiToBo.core.datatypes.images.MTBImage;
 import de.unihalle.informatik.MiToBo.io.images.ImageWriterMTB;
 import de.unihalle.informatik.MiToBo.io.tools.FilePathManipulator;
-import de.unihalle.informatik.MiToBo.tools.image.ImageConverter;
 import de.unihalle.informatik.rhizoTrak.Project;
 import de.unihalle.informatik.rhizoTrak.display.*;
-import de.unihalle.informatik.rhizoTrak.display.Treeline.RadiusNode;
 import de.unihalle.informatik.rhizoTrak.tree.ProjectThing;
 import de.unihalle.informatik.rhizoTrak.utils.Utils;
 import ij.ImagePlus;
-import org.python.core.imp;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Point2D;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 /** Write the annotaition as a binary image
  *
@@ -104,15 +102,15 @@ public class RhizoWriteBinary {
 	private final String ONLY_STRING = "Current layer only";
 	private final String ALL_STRING = "All layers";
 
-	final int whiteColor = 0x0000ff;
+	/**
+	 * color for foreground pixels
+	 */
+	final int fgColor = 0x0000ff;
 
 	private RhizoMain rhizoMain;
 
 	private boolean debug = false;
 
-	// Hash of all layers with treeline(s) to write to
-	// set in computeStatistics
-	private HashSet<Layer> allLayers = null;
 	private MTBImage image;
 
 	public RhizoWriteBinary(RhizoMain rhizoMain) 	{
@@ -134,15 +132,14 @@ public class RhizoWriteBinary {
 		JPanel choicesPanel = new JPanel();
 		choicesPanel.setLayout(new GridLayout( 2, 1, 0, 10));
 
-//		 only current layer at the moment
-//		choicesPanel.add(new JLabel("Layers "));
-//		JComboBox<String> layerCombo = new JComboBox<String>(choices2);
-//		choicesPanel.add( layerCombo);
+		choicesPanel.add(new JLabel("Layers "));
+		JComboBox<String> layerCombo = new JComboBox<String>(choices2);
+		choicesPanel.add( layerCombo);
 
 		choicesPanel.add(new JLabel("Status labels "));
 		String[] statusLabelChoices = new String[rhizoMain.getProjectConfig().sizeStatusLabelMapping()];
 		for ( int i = 0 ; i < rhizoMain.getProjectConfig().sizeStatusLabelMapping() ; i++ ) {
-			statusLabelChoices[i] = rhizoMain.getProjectConfig().getStatusLabel(i).getName();
+			statusLabelChoices[i] = i + "(" + rhizoMain.getProjectConfig().getStatusLabel(i).getName() + ")";
 		}
 		JList statusLabelCombo = new JList( statusLabelChoices);
 		statusLabelCombo.setSelectedIndex( 0);
@@ -154,87 +151,70 @@ public class RhizoWriteBinary {
 			return;
 		}
 
+		String outputLayers = (String) layerCombo.getSelectedItem();
 		for ( int i : statusLabelCombo.getSelectedIndices() ) {
 			statusLabelIntToWrite.add( (byte) i);
 		}
 
-//		 only current layer at the moment
-//		boolean drawAnnotationsAllLayers = outputLayers.equals( ALL_STRING);
-		boolean drawAnnotationsAllLayers = false;
+		boolean drawAnnotationsAllLayers = outputLayers.equals( ALL_STRING);
 
-		// Select and open and output file
-		String basefilename = rhizoMain.getXmlName().replaceFirst(".xml\\z", "");
-
-		String folder;
-		if  ( rhizoMain.getStorageFolder() == null )
-			folder = System.getProperty("user.home");
-		else
-			folder = rhizoMain.getStorageFolder();
-
-		Path imagePath = null;
-		if ( drawAnnotationsAllLayers ) {
-			// will not hapen currently
+		// find all rootstacks
+		HashSet<ProjectThing> rootstackThings = RhizoUtils.getRootstacks( rhizoMain.getProject());
+		if ( rootstackThings == null) {
+			Utils.showMessage( "WriteBinary warning: no rootstack found");
 			return;
-		} else {
-			// create a suggestion for the image filename
-			imagePath = Paths.get( Display.getFront().getLayer().getPatches( false).get(0).getImageFilePath());
-			String basename = FilePathManipulator.removeExtension( imagePath.toString());
-			String extension = FilePathManipulator.getExtension(  imagePath.toString());
-			String filenameProposal = basename+"-binary."+extension;
-
-			// ask for the image name
-			JFileChooser fileChooser = new JFileChooser();
-			FileNameExtensionFilter filter = new FileNameExtensionFilter( "File for writing annotations a binary image", "tif");
-			fileChooser.setFileFilter(filter);
-			fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-			fileChooser.setDialogTitle("File to write binary annotation to");
-			fileChooser.setSelectedFile(new File( filenameProposal));
-			int returnVal = fileChooser.showOpenDialog(null);
-
-			if (returnVal != JFileChooser.APPROVE_OPTION)
-				return; // user cancelled dialog
-
-			File saveFile = fileChooser.getSelectedFile();
-			imagePath = saveFile.toPath();
 		}
 
-		ImagePlus ip = Display.getFront().getLayer().getPatches(true).get(0).getImagePlus();
-		System.out.println( "IP " + ip.getCalibration());
-		MTBImage image = MTBImage.createMTBImage(ip);
-		System.out.println( "image " + image.getCalibration());
-
-		// need to get calibration info and so on
-//		MTBImage binaryImage = MTBImage.createMTBImage( image.getSizeX(), image.getSizeY(), image.getSizeZ(), image.getSizeT(), image.getSizeC(), MTBImage.MTBImageType.MTB_BYTE);
-
-		MTBImage binaryImage = null;
-
-		try {
-			ImageConverter imgConv = new ImageConverter( image, MTBImage.MTBImageType.MTB_BYTE, false, false);
-			imgConv.runOp( true);
-			binaryImage = imgConv.getResultImg();
-			System.out.println( "binaryImage " + binaryImage.getCalibration().info);
-
-		} catch (ALDOperatorException e) {
-			e.printStackTrace();
-		} catch (ALDProcessingDAGException e) {
-			e.printStackTrace();
-		}
-		ImagePlus imp = binaryImage.getImagePlus();
 
 		if ( drawAnnotationsAllLayers ) {
-			// should not happen currently
+			for ( Layer layer : rhizoMain.getProject().getRootLayerSet().getLayers() ) {
+				drawLayer( layer, rootstackThings, statusLabelIntToWrite);
+			}
 		} else {
-			drawSegments( Display.getFront().getProject(), Display.getFront().getLayer(), imp, statusLabelIntToWrite);
+			drawLayer( Display.getFront().getLayer(), rootstackThings, statusLabelIntToWrite);
 		}
+
+	}
+
+	/** draw <code>layer</code> using all status labels in <code>statusLabelIntToWrite</code>
+	 *  @param layer
+	 * @param rootstackThings
+	 * @param statusLabelIntToWrite
+	 */
+	private void drawLayer(Layer layer, HashSet<ProjectThing> rootstackThings, ArrayList<Byte> statusLabelIntToWrite) {
+		// create a suggestion for the image filename
+		Path imagePath = Paths.get( layer.getPatches( false).get(0).getImageFilePath());
+		String basename = FilePathManipulator.removeExtension( imagePath.toString());
+		String extension = FilePathManipulator.getExtension(  imagePath.toString());
+		String filenameProposal = basename+"-binary."+extension;
+
+		// ask for the image name
+		JFileChooser fileChooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter( "File for writing annotations a binary image", "tif");
+		fileChooser.setFileFilter(filter);
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		fileChooser.setDialogTitle("File to write binary annotation to");
+		fileChooser.setSelectedFile(new File( filenameProposal));
+		int returnVal = fileChooser.showOpenDialog(null);
+
+		if (returnVal != JFileChooser.APPROVE_OPTION)
+			return; // user cancelled dialog
+
+		File saveFile = fileChooser.getSelectedFile();
+		imagePath = saveFile.toPath();
+
+		ImagePlus ip = layer.getPatches(true).get(0).getImagePlus();
+		MTBImage image = MTBImage.createMTBImage(ip);
+
+		MTBImage binaryImage = MTBImage.createMTBImage( image.getSizeX(), image.getSizeY(), image.getSizeZ(), image.getSizeT(), image.getSizeC(), MTBImage.MTBImageType.MTB_BYTE);
+
+		drawSegments( rhizoMain.getProject(), layer, binaryImage.getImagePlus(), rootstackThings, statusLabelIntToWrite);
 
 		try {
 			ImageWriterMTB writer = new ImageWriterMTB(binaryImage, imagePath.toString());
 			writer.runOp( true);
-		} catch (final ALDOperatorException ex) {
+		} catch (final ALDOperatorException | ALDProcessingDAGException ex) {
 			Utils.showMessage( "cannot write binary annotation image to  " + imagePath.toString());
-			ex.printStackTrace();
-		}		 catch (final ALDProcessingDAGException ex) {
-			Utils.showMessage("cannot write binary annotation image to  " + imagePath.toString());
 			ex.printStackTrace();
 		}
 	}
@@ -243,22 +223,14 @@ public class RhizoWriteBinary {
 	 * @param project
 	 * @param currentLayer if null all layers are considered*
 	 * @param imp image processor of the binary image
+	 * @param rootstackThings
 	 * @return list of all <code>Segment</code>s to write, null on failure
 	 */
-	private void drawSegments(Project project, Layer currentLayer, ImagePlus imp, ArrayList<Byte> statusLabelIntToWrite) {
+	private void drawSegments(Project project, Layer currentLayer, ImagePlus imp, HashSet<ProjectThing> rootstackThings, ArrayList<Byte> statusLabelIntToWrite) {
 		// all treelines below a rootstack
 		LinkedList<Treeline> allTreelines = new LinkedList<Treeline>();
 
-		// find all rootstacks
-		HashSet<ProjectThing> rootstackThings = RhizoUtils.getRootstacks( project);
-		if ( rootstackThings == null) {
-			Utils.showMessage( "WriteStatistics warning: no rootstack found");
-			return;
-		}
-
-		// and collect treelines and connectors below all rootstacks
-		this.allLayers = new HashSet<Layer>(); // all layers we have a treeline in to write
-
+		// collect treelines and connectors below all rootstacks
 		for ( ProjectThing rootstackThing :rootstackThings ) {
 			if ( debug)	System.out.println("rootstack " + rootstackThing.getId());
 			for ( ProjectThing pt : rootstackThing.findChildrenOfTypeR( Treeline.class)) {
@@ -271,7 +243,6 @@ public class RhizoWriteBinary {
 							( currentLayer == null || currentLayer.equals( tl.getFirstLayer())) ) {
 						if ( debug)	System.out.println( "           as treeline");
 						allTreelines.add(tl);
-						allLayers.add( tl.getFirstLayer());
 					}
 				}
 			}
@@ -304,11 +275,13 @@ public class RhizoWriteBinary {
 						int cy2 = (int) p2.getY();
 
 						float startRadius = node.getParent().getData();
+						if ( startRadius < 1 )  startRadius = 1;
 						float endRadius = node.getData();
+						if ( endRadius < 1 ) endRadius = 1;
 
-						Drawing.drawSegment(imp, cx1, cy1, cx2, cy2, (int)startRadius, (int)endRadius, whiteColor);
-						Drawing.drawFilledCircle(imp, cx1, cy1, startRadius, whiteColor);
-						Drawing.drawFilledCircle(imp, cx2, cy2, endRadius, whiteColor);
+						Drawing.drawSegment(imp, cx1, cy1, cx2, cy2, (int)startRadius, (int)endRadius, fgColor);
+						Drawing.drawFilledCircle(imp, cx1, cy1, startRadius, fgColor);
+						Drawing.drawFilledCircle(imp, cx2, cy2, endRadius, fgColor);
 
 						imp.updateAndDraw();
 					}
